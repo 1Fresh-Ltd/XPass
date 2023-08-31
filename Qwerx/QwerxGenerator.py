4cde305bfd66d16c7eb0d2c5fe82ab61c78ac545
unrealToffi <143597781+untoffi@users.noreply.github.com>
2023-08-31 11:16:39 +0300

    Update QwerxGenerator.py

--- Qwerx/QwerxGenerator.py
+++ Qwerx/QwerxGenerator.py
@@ -16,15 +16,5 @@ pwd = ''
 for i in range(pwd_length):
   pwd += ''.join(secrets.choice(alphabet))
 
-print(pwd)
-
-
-while True:
-  pwd = ''
-  for i in range(pwd_length):
-    pwd += ''.join(secrets.choice(alphabet))
-
-  if (any(char in special_chars for char in pwd) and 
-      sum(char in digits for char in pwd)>=2):
-          break
-print(pwd)
+print("Generated password is : " + pwd)
+time.sleep( 10 )
